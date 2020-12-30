124250babbdb214f65f683604060f3108a40be5b
Tam Phan <tam.phan@mac.com>
2020-12-29 16:37:55 -0800

    Implemented image loading

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Tam Phan on 12/24/20.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -27,5 +27,28 @@ class ViewController: UITableViewController {
         
         print(pictures)
     }
+    
+    // Dynamically sets the number of rows by counting items in array
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    // When scrolling, dequeues unseen cells and creates a new one
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typcasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedImage = pictures[indexPath.row]
+            
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
